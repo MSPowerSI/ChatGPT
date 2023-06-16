7db957919ee73d15148d82fe50fbd9b273df2c7a
renanalmeida_billy@hotmail.com <renanalmeida@MacBook-Pro-de-Renan.local>
2023-06-16 11:07:18 -0300

    Ajuste local storage

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import session from 'express-session';
 dotenv.config();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -19,64 +19,68 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-// Carregar as informações iniciais do arquivo config.json
+// Load initial information from config.json file
 const initialConfig = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 let messages = [];
 
 if (initialConfig.companyName) {
-  messages.push({ role: 'system', content: `Nome da empresa: ${initialConfig.companyName}` });
+    messages.push({ role: 'system', content: `Company Name: ${initialConfig.companyName}` });
 }
 
 if (initialConfig.ownerName) {
-  messages.push({ role: 'system', content: `Nome do proprietário: ${initialConfig.ownerName}` });
+    messages.push({ role: 'system', content: `Owner Name: ${initialConfig.ownerName}` });
 }
 
-// Adicionar funções como mensagens do sistema
+// Add functions as system messages
 if (initialConfig.functions && Array.isArray(initialConfig.functions)) {
-  for (const func of initialConfig.functions) {
-    messages.push({ role: 'system', content: `Função: ${func}` });
-  }
+    for (const func of initialConfig.functions) {
+        messages.push({ role: 'system', content: `Function: ${func}` });
+    }
 }
 
-// Adicionar regras como mensagens do sistema
+// Add rules as system messages
 if (initialConfig.rules && Array.isArray(initialConfig.rules)) {
-  for (const rule of initialConfig.rules) {
-    messages.push({ role: 'system', content: `Regra: ${rule}` });
-  }
+    for (const rule of initialConfig.rules) {
+        messages.push({ role: 'system', content: `Rule: ${rule}` });
+    }
 }
-
-app.get('/', (req, res) => {
-  res.render('front', { messages: req?.session?.messages ? req.session.messages.filter(message => message.role !== 'system') : [] });
-});
 
 app.use(session({
-  secret: process.env.SECRET_KEY,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
 }));
 
+app.get('/', (req, res) => {
+    // Load the chat messages from the session
+    req.session.messages = req.session.messages || messages;
+
+    res.render('front', { messages: req.session.messages.filter(message => message.role !== 'system') });
+});
+
 async function getAssistantMessage(messages) {
-  const completion = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: messages,
-  });
-  return completion.data.choices[0].message.content;
+    const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: messages,
+    });
+
+    return completion.data.choices[0].message.content;
 }
 
 app.post('/chat', async (req, res) => {
-  console.log(req?.session);
-  const userMessage = req.body.content;
-  if (!req.session.messages) {
-    req.session.messages = messages;
-  }
-  req.session.messages.push({ role: 'user', content: userMessage });
-  const assistantMessage = await getAssistantMessage(req.session.messages);
-  req.session.messages.push({ role: 'assistant', content: assistantMessage });
-  res.json({ content: assistantMessage });
+    const userMessage = req.body.content;
+
+    req.session.messages.push({ role: 'user', content: userMessage });
+
+    const assistantMessage = await getAssistantMessage(req.session.messages);
+
+    req.session.messages.push({ role: 'assistant', content: assistantMessage });
+
+    res.json({ content: assistantMessage });
 });
 
 app.listen(port, () => {
-  console.log(`Chatbot rodando na porta ${port}`);
+    console.log(`Chatbot running on port ${port}`);
 });
